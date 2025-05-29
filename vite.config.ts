1721b0506514681a4739ddac29abe0e1015c071b
sadek <emon01889412738@gmail.com>
2025-05-29 19:46:31 +0600

    commit4th

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-        page2: path.resolve(__dirname, 'side.html'),
-      },
-    },
-  },
+
 })
